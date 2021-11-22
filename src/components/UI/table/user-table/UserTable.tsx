c9794bd5b48c4f100b6dcf1902517eb77436e571
unknown <hotien2107@gmail.com>
2021-11-22 14:23:51 +0700

    list teachers and students

--- src/components/UI/table/user-table/UserTable.tsx
+++ src/components/UI/table/user-table/UserTable.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Avatar from "../../avatar/Avatar";
 import Pagination from "../../pagination/Pagination";
 
@@ -14,12 +14,26 @@ type userTableProps = {
   memberIdChoose: number;
 };
 
+const MEMBERS_PER_PAGE = 6;
+
 const UserTable = ({
   listUsers,
   onChooseMember,
   memberIdChoose,
 }: userTableProps) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [listMembersPerPage, setListMembersPerPage] = useState<user[]>([]);
+
+  useEffect(() => {
+    let listMembersInCurrentPage: user[] = [];
+    if (listUsers.length > 0) {
+      listMembersInCurrentPage = listUsers.slice(
+        (currentPage - 1) * MEMBERS_PER_PAGE,
+        currentPage * MEMBERS_PER_PAGE
+      );
+    }
+    setListMembersPerPage(listMembersInCurrentPage);
+  }, [currentPage, listUsers]);
 
   const chooseMemberHandle = (id: number) => {
     onChooseMember(id);
@@ -39,30 +53,37 @@ const UserTable = ({
           </tr>
         </thead>
         <tbody>
-          {listUsers.map((user) => {
-            return (
-              <tr
-                className={
-                  "user-table__row" +
-                  (memberIdChoose === user.id ? " row-active" : "")
-                }
-                key={user.id}
-                onClick={() => chooseMemberHandle(user.id)}
-              >
-                <td className="user-table__name">
-                  <Avatar imageSrc={user.avatar} /> <span>{user.fullName}</span>
-                </td>
-                <td className="user-table__email">nguyenvana@gmail.com</td>
-                <td className="user-table__join-date">20/11/2021</td>
-              </tr>
-            );
-          })}
+          {listMembersPerPage.length === 0 ? (
+            <tr>
+              <td>Không có thành viên nào</td>
+            </tr>
+          ) : (
+            listMembersPerPage.map((user) => {
+              return (
+                <tr
+                  className={
+                    "user-table__row" +
+                    (memberIdChoose === user.id ? " row-active" : "")
+                  }
+                  key={user.id}
+                  onClick={() => chooseMemberHandle(user.id)}
+                >
+                  <td className="user-table__name">
+                    <Avatar imageSrc={user.avatar} />{" "}
+                    <span>{user.fullName}</span>
+                  </td>
+                  <td className="user-table__email">nguyenvana@gmail.com</td>
+                  <td className="user-table__join-date">20/11/2021</td>
+                </tr>
+              );
+            })
+          )}
         </tbody>
       </table>
 
       <Pagination
-        totalItem={50}
-        itemPerPage={7}
+        totalItem={listUsers.length}
+        itemPerPage={MEMBERS_PER_PAGE}
         pageActive={currentPage}
         setPageActive={onChangeCurrentPage}
       />
