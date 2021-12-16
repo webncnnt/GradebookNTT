c0cda9e6b7bb9aa9bdda75c2fc8712f438cc249e
Ho Tien <hotien2107@gmail.com>
2021-12-16 20:30:31 +0700

    add react-csv

--- src/pages/class-detail/members/Members.tsx
+++ src/pages/class-detail/members/Members.tsx
@@ -1,10 +1,15 @@
-import { useEffect, useState } from "react";
-import AddIcon from "../../../components/icons/Add";
-import Container from "../../../components/layouts/container/Container";
-import InviteMemberForm from "../../../components/UI/form/invite-members/InviteMemberForm";
-import UserTable from "../../../components/UI/table/user-table/UserTable";
-import useHttp from "../../../hooks/useHttp";
-import MemberDetail from "./members-detail/MemberDetail";
+import { useEffect, useState } from 'react';
+import CSVReader from 'react-csv-reader';
+import { CSVLink } from 'react-csv';
+
+import AddIcon from '../../../components/icons/Add';
+import UploadIcon from '../../../components/icons/Upload';
+import Container from '../../../components/layouts/container/Container';
+import InviteMemberForm from '../../../components/UI/form/invite-members/InviteMemberForm';
+import UserTable from '../../../components/UI/table/user-table/UserTable';
+import useHttp from '../../../hooks/useHttp';
+import MemberDetail from './members-detail/MemberDetail';
+import DownloadIcon from '../../../components/icons/Download';
 
 interface memberInfo {
   id: number;
@@ -14,6 +19,17 @@ interface memberInfo {
   joinDate: string;
 }
 
+const csv_headers = [
+  { label: 'Tên', key: 'studentName' },
+  { label: 'MSSV', key: 'studentID' },
+];
+
+const csv_data = [
+  { studentName: 'Ahmed', studentID: '123' },
+  { studentName: 'ádg', studentID: '134' },
+  { studentName: 'Ahyq3med', studentID: '876' },
+];
+
 const Members = () => {
   const [listTeachers, setListTeachers] = useState<memberInfo[]>([]);
   const [listStudents, setListStudents] = useState<memberInfo[]>([]);
@@ -21,22 +37,20 @@ const Members = () => {
   const [memberIdDetail, setMemberIdDetail] = useState<number>(0);
   const [isShowInviteForm, setIsShowInviteForm] = useState<boolean>(false);
 
-  const {error, sendRequest} = useHttp();
+  const { error, sendRequest } = useHttp();
 
   const pathname = window.location.pathname;
 
   useEffect(() => {
     const requestConfig = {
-      url: "classes/" +
-      pathname.split("/")[2] +
-      "/teachers"
-    }
+      url: 'classes/' + pathname.split('/')[2] + '/teachers',
+    };
     const handleError = () => {
       console.log(error);
-    }
-    
+    };
+
     const getTeachers = (data: any) => {
-      console.log("Get teacher profile");
+      console.log('Get teacher profile');
       const memberInfoFormat = data.data.teachers.map((member: any) => {
         return {
           id: member.profile.id,
@@ -47,21 +61,19 @@ const Members = () => {
         };
       });
       setListTeachers(memberInfoFormat);
-    }     
+    };
     sendRequest(requestConfig, handleError, getTeachers);
   }, [pathname, error, sendRequest]);
 
   useEffect(() => {
-    console.log("Get students profile");
+    console.log('Get students profile');
     const requestConfig = {
-      url: "classes/" +
-      pathname.split("/")[2] +
-      "/students"
-    }
+      url: 'classes/' + pathname.split('/')[2] + '/students',
+    };
     const handleError = () => {
       console.log(error);
-    }
-    
+    };
+
     const getStudents = (data: any) => {
       const memberInfoFormat = data.data.students.map((member: any) => {
         return {
@@ -73,7 +85,7 @@ const Members = () => {
         };
       });
       setListStudents(memberInfoFormat);
-    }     
+    };
     sendRequest(requestConfig, handleError, getStudents);
   }, [pathname, error, sendRequest]);
 
@@ -81,57 +93,65 @@ const Members = () => {
     setMemberIdDetail(id);
   };
 
+  const papaparseOptions = {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+  };
+
+  const handleForce = (data: any, fileInfo: any) => console.log(data, fileInfo);
+
   return (
     <Container>
-      {isShowInviteForm ? (
-        <InviteMemberForm onClose={() => setIsShowInviteForm(false)} />
-      ) : null}
-      <div className="members">
-        <div className="members__content">
-          <div className="members__title-invite">
-            <h1 className="members__title">Thành viên</h1>
-            <AddIcon
-              className="frame"
-              onClick={() => setIsShowInviteForm(true)}
-            />
+      {isShowInviteForm ? <InviteMemberForm onClose={() => setIsShowInviteForm(false)} /> : null}
+      <div className='members'>
+        <div className='members__content'>
+          <div className='members__title-invite'>
+            <h1 className='members__title'>Thành viên</h1>
+            <AddIcon className='frame' onClick={() => setIsShowInviteForm(true)} />
           </div>
-          <div className="members__table">
-            <ul className="members__menu">
-              <li
-                className={
-                  "members__menu-item" + (isShowListTeacher ? " active" : "")
-                }
-                onClick={() => setIsShowListTeacher(true)}
-              >
+          <div className='members__table'>
+            <ul className='members__menu'>
+              <li className={'members__menu-item' + (isShowListTeacher ? ' active' : '')} onClick={() => setIsShowListTeacher(true)}>
                 Giảng viên
               </li>
-              <li
-                className={
-                  "members__menu-item" + (!isShowListTeacher ? " active" : "")
-                }
-                onClick={() => setIsShowListTeacher(false)}
-              >
+              <li className={'members__menu-item' + (!isShowListTeacher ? ' active' : '')} onClick={() => setIsShowListTeacher(false)}>
                 Học viên
               </li>
             </ul>
-            <div className="members__list">
+            <div className='members__list'>
               {isShowListTeacher ? (
-                <UserTable
-                  listUsers={listTeachers}
-                  onChooseMember={chooseMember}
-                  memberIdChoose={memberIdDetail}
-                />
+                <UserTable listUsers={listTeachers} onChooseMember={chooseMember} memberIdChoose={memberIdDetail} />
               ) : (
-                <UserTable
-                  listUsers={listStudents}
-                  onChooseMember={chooseMember}
-                  memberIdChoose={memberIdDetail}
-                />
+                <UserTable listUsers={listStudents} onChooseMember={chooseMember} memberIdChoose={memberIdDetail} />
               )}
+            </div>
+            <div className='members__downdoad-upload'>
+              <div className='members__download'>
+                {!isShowListTeacher ? (
+                  <CSVLink data={csv_data} filename={'list-students.csv'} headers={csv_headers}>
+                    <DownloadIcon className='btn btn--primary icon--white' />
+                  </CSVLink>
+                ) : null}
+                ;
+              </div>
+              <div className='member__upload'>
+                {!isShowListTeacher ? (
+                  <CSVReader
+                    cssClass='csv-reader-input'
+                    label={<UploadIcon className='btn btn--primary icon--white' />}
+                    onFileLoaded={handleForce}
+                    parserOptions={papaparseOptions}
+                    inputId='ObiWan'
+                    inputName='ObiWan'
+                    inputStyle={{ color: 'red' }}
+                  />
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
-        <div className="members__detail">
+        <div className='members__detail'>
           <MemberDetail memberId={memberIdDetail} />
         </div>
       </div>
