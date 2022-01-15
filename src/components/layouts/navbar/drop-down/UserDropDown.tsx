8e06b7b8f27ba16dfa730ec2da33481708253b45
Ho Tien <hotien2107@gmail.com>
2022-01-15 19:42:59 +0700

    merge new code

--- src/components/layouts/navbar/drop-down/UserDropDown.tsx
+++ src/components/layouts/navbar/drop-down/UserDropDown.tsx
@@ -1,22 +1,26 @@
-import { Link } from "react-router-dom"
-import { useAuth } from "../../../../contexts/auth-context"
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../../contexts/auth-context";
 
 type dropdownProps = {
-    onClick: () => void;
-}
+  onClick: () => void;
+};
 
-const UserDropDown = ({onClick}: dropdownProps) => {
-    const authCtx = useAuth();
-    return (
-        <div className="drop-down">
-            <ul className="drop-down__items" onClick={onClick}>
-                <li className="drop-down__item"><Link to="/user-detail">Thông tin cá nhân</Link></li>
-                <li className="drop-down__item">Lớp giảng dạy</li>
-                <li className="drop-down__item">Lớp tham gia</li>
-                <li className="drop-down__item" onClick={() => authCtx.onLogout()}>Đăng xuất</li>
-            </ul>
-        </div>
-    )
-}
+const UserDropDown = ({ onClick }: dropdownProps) => {
+  const authCtx = useAuth();
+  return (
+    <div className='drop-down'>
+      <ul className='drop-down__items' onClick={onClick}>
+        <li className='drop-down__item'>
+          <Link to='/user-detail'>Thông tin cá nhân</Link>
+        </li>
+        <li className='drop-down__item'>Lớp giảng dạy</li>
+        <li className='drop-down__item'>Lớp tham gia</li>
+        <li className='drop-down__item' onClick={() => authCtx.onLogout()}>
+          Đăng xuất
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default UserDropDown
+export default UserDropDown;
